aa0ce95d6308aff5d369cfff7eb93a43463f4ce5
sumanamys <sumana.siddalingappa@gmail.com>
2020-05-14 16:46:48 -0500

    servicediscovery3

--- applications/allocations-server/src/main/java/io/pivotal/pal/tracker/allocations/App.java
+++ applications/allocations-server/src/main/java/io/pivotal/pal/tracker/allocations/App.java
@@ -14,6 +14,7 @@ import java.util.TimeZone;
 @SpringBootApplication
 @ComponentScan({"io.pivotal.pal.tracker.allocations", "io.pivotal.pal.tracker.restsupport"})
 @EnableEurekaClient
+
 public class App {
 
     public static void main(String[] args) {
