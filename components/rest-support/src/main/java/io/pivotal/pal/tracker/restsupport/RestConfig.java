2231436de1ed7d03d5e5ca01c744d674f310f372
sumanamys <sumana.siddalingappa@gmail.com>
2020-05-14 21:55:19 -0500

    Secure Distributed system

--- components/rest-support/src/main/java/io/pivotal/pal/tracker/restsupport/RestConfig.java
+++ components/rest-support/src/main/java/io/pivotal/pal/tracker/restsupport/RestConfig.java
@@ -11,7 +11,7 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class RestConfig {
 
-    @Bean
+
     @LoadBalanced
     public RestOperations restOperations() {
         return new RestTemplate();
